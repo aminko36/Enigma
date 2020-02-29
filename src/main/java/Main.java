286db4ed75e98e091fa331467a11b4e61725047b
AnetaWM <aneta102@wp.pl>
2020-02-29 13:36:58 +0100

    Task-7.Saving to file and loading to file content implemented.Project structure refactory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,16 +6,22 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    private static Stage mainStage;
     String resourcesPath = "/fxml/Main.fxml";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource(resourcesPath));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         primaryStage.setTitle("Enigma");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static Stage getMainStage() {
+        return mainStage;
     }
 }
 
--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -1,12 +1,16 @@
 package controllers;
 
 import ciphers.Cipher;
-        import ciphers.impl.CesarCipher;
-        import javafx.fxml.FXML;
-        import javafx.fxml.Initializable;
-        import javafx.scene.control.TextArea;
-        import java.net.URL;
-        import java.util.ResourceBundle;
+import ciphers.impl.CesarCipher;
+import file.utils.FileTool;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class MainController implements Initializable {
@@ -15,27 +19,63 @@ public class MainController implements Initializable {
     }
 
     @FXML
-    TextArea textArea;
+    TextArea inputTextArea;
 
     @FXML
     public void triggerEncoding() {   //metoda dla Buttona
 
-        String userText = textArea.getText();
+        String userText = inputTextArea.getText();
         if (!userText.isEmpty()) {
             Cipher cesarCipher = new CesarCipher();
             String encode = cesarCipher.encode(userText);
-            textArea.setText(encode);
+            inputTextArea.setText(encode);
         }
     }
+
     @FXML
     public void triggerDecoding() {
-        String userText = textArea.getText();
+        String userText = inputTextArea.getText();
         if (!userText.isEmpty()) {
             Cipher cesarCipher = new CesarCipher();
             String decode = cesarCipher.decode(userText);
-            textArea.setText(decode);
+            inputTextArea.setText(decode);
+        }
+    }
+
+    @FXML
+    public void saveToFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save to file");
+        fileChooser.setInitialFileName("message.crpt");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("crypt file", "*.crpt"),
+                new FileChooser.ExtensionFilter("text file", "*.txt")
+        );
+        File fileToSave = fileChooser.showSaveDialog(null);
+        if (fileToSave != null) {
+            String absolutePath = fileToSave.getAbsolutePath();
+
+            FileTool.writeFileContent(absolutePath, inputTextArea.getText());
+
+        }
+    }
+
+    @FXML
+    public void loadFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Load file");
+        fileChooser.setInitialFileName("message.crpt");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("crypt file", "*.crpt"),
+                new FileChooser.ExtensionFilter("text file", "*.txt")
+        );
+        File fileToLoad = fileChooser.showOpenDialog(null);
+        if (null != fileToLoad) {
+            String fileContent = FileTool.getFileContent(fileToLoad.getAbsolutePath());
+            inputTextArea.setText(fileContent);
+        }
     }
 }
 
 
-}
+
